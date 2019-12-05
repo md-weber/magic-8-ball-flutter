b360cafe694c2353ce67d6fdc3c93edfeeb2f22c
myracledesign <weber.max.1988@live.de>
2019-12-05 22:14:33 +0100

    finished the 8ball game

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade500,
+      appBar: AppBar(
+        title: Text("Ask me anything!"),
+        backgroundColor: Colors.blueGrey.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Center(
+        child: Image.asset("images/ball$ballNumber.png"),
+      ),
+    );
+  }
+}
